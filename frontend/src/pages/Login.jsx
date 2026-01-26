176464b88baf0d0ae632fc50b696bfb65ed050d0
Soraya <1csorayaturegano@gmail.com>
2026-01-26 01:09:51 +0100

    creacion del login y registro de usuarios correcto

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/FencePro_Logo.png';
+import './Login.css';
+
 
 function Login() {
   const handleSubmit = (e) => {
@@ -14,7 +16,7 @@ function Login() {
         <img src={logo} alt="FencePro" className="auth-logo" />
 
 
-        <h2>Bienvenido</h2>
+        <h2>Bienvenid@</h2>
 
         <form onSubmit={handleSubmit}>
           <input
@@ -28,6 +30,17 @@ function Login() {
             placeholder="Contraseña"
             required
           />
+          <div className="auth-forgot">
+            <span
+              className="forgot-link"
+              onClick={() => {
+              console.log("Recuperar contraseña");
+              }}
+            >
+              ¿Has olvidado la contraseña?
+            </span>
+          </div>
+
 
           <button type="submit">Iniciar sesión</button>
         </form>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,61 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/FencePro_Logo.png';
+import './Login.css'; 
+
 function Register() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Registro enviado');
+  };
+
   return (
-    <div>
-      <h1>REGISTER FUNCIONA</h1>
+    <div className="auth-container">
+      <div className="auth-card">
+
+        <img src={logo} alt="FencePro" className="auth-logo" />
+
+        <h2>Bienvenid@</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="row-inputs">
+            <input type="text" placeholder="Nombre" required/>
+            <input type="text" placeholder="Apellidos" required/>
+          </div>
+
+          <input type="email" placeholder="Email" required/>
+
+          <input type="password" placeholder="Contraseña" required/>
+
+          <div className="row-inputs">
+            <input type="date" placeholder="Fecha de nacimiento" required/>
+
+            <select required>
+              <option value="">Selecciona tu rol</option>
+              <option value="deportista">Deportista</option>
+              <option value="entrenador">Entrenador</option>
+              <option value="club">Club</option>
+              <option value="arbitro">Árbitro</option>
+              <option value="administrador">Admin</option>
+            </select>
+          </div>
+
+          <input type="text" placeholder="Club"/>
+          
+          <div className="auth-checkbox">
+            <input type="checkbox" required />
+            <span>
+              He leído y acepto la <a href="#">política de privacidad y cookies</a>
+            </span>
+          </div>
+
+          <button type="submit">Crear cuenta</button>
+        </form>
+
+        <div className="auth-link">
+          <Link to="/">Iniciar sesión</Link>
+        </div>
+
+      </div>
     </div>
   );
 }
