ff99960fec4891e1d2f466ead943ede2676fd015
Sandra Marcos <smarcosg90@gmail.com>
2026-02-04 18:58:02 +0100

    Implementación endpoint /api/deportistas para mostrar listado deportistas

--- backend/src/main/java/com/fencepro/backend/controller/DeportistaController.java
+++ backend/src/main/java/com/fencepro/backend/controller/DeportistaController.java
@@ -1,11 +1,14 @@
 package com.fencepro.backend.controller;
 
 import com.fencepro.backend.dto.RegistroDeportistaRequest;
+import com.fencepro.backend.dto.response.DeportistaPerfilResponse;
 import com.fencepro.backend.service.DeportistaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/deportistas")
@@ -25,6 +28,16 @@ public class DeportistaController {
 
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Error en el registro: " + e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DeportistaPerfilResponse>> listarDeportistas() {
+        try {
+            List<DeportistaPerfilResponse> lista = deportistaService.listarTodos();
+            return ResponseEntity.ok(lista);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().build();
         }
     }
 }
