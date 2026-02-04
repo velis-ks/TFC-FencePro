bd5e51fd6238f81bfd2b1291943213555758e1ff
Sandra Marcos <smarcosg90@gmail.com>
2026-02-04 17:04:36 +0100

    Servicio transaccional Usuario+Entrenador

---
+++ backend/src/main/java/com/fencepro/backend/service/ArbitroService.java
@@ -0,0 +1,71 @@
+package com.fencepro.backend.service;
+
+import com.fencepro.backend.dto.RegistroArbitroRequest;
+import com.fencepro.model.entity.Arbitro;
+import com.fencepro.model.entity.Usuario;
+import com.fencepro.model.enums.NivelArbitro;
+import com.fencepro.model.enums.NivelTecnico;
+import com.fencepro.model.enums.Rol;
+import com.fencepro.repository.ArbitroRepository;
+import com.fencepro.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+
+public class ArbitroService {
+    private final UsuarioRepository usuarioRepository;
+    private final ArbitroRepository arbitroRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Arbitro registrarArbitro(RegistroArbitroRequest request){
+        //Validación email+dni
+        if(usuarioRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("El email ya está registrado");
+        }
+
+        if(arbitroRepository.findByDni(request.getDni()).isPresent()){
+            throw new RuntimeException("El DNI ya existe en el sistema");
+        }
+
+        //enum
+        NivelArbitro nivel;
+        try{
+            nivel = NivelArbitro.valueOf(request.getNivel().toUpperCase());
+        }catch(IllegalArgumentException e){
+            throw new RuntimeException("Nivel no válidos. Valores admitidos: AUTONOMICO, NACIONAL, INTERNACIONAL");
+        }
+
+        //Creación usuario
+        Usuario usuario = Usuario.builder()
+                .nombre(request.getNombre())
+                .apellidos(request.getApellidos())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .rol(Rol.ARBITRO)
+                .telefono(request.getTelefono())
+                .activo(true)
+                .emailVerificado(false)
+                .build();
+
+        usuario = usuarioRepository.save(usuario);
+
+        //Ficha de árbitro
+        Arbitro arbitro = new Arbitro();
+        arbitro.setUsuario(usuario);
+        arbitro.setDni(request.getDni());
+        arbitro.setFechaNacimiento(request.getFechaNacimiento());
+        arbitro.setNumeroLicencia(request.getNumeroLicencia());
+        arbitro.setNivel(nivel);
+        arbitro.setFechaCertificacion(LocalDate.now());
+        arbitro.setCompeticionesArbitradas(0);
+
+        return arbitroRepository.save(arbitro);
+    }
+}
